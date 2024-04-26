3e2945c82a0c90313c73a88346abe00adcc0c781
Yang Hau <yuanyanghau@gmail.com>
2024-04-26 14:51:16 +0200

    fix the typos

--- rollups/sovereign-labs/movement-sovereign-labs-rollup/modules/sov-aptosvm/src/lib.rs
+++ rollups/sovereign-labs/movement-sovereign-labs-rollup/modules/sov-aptosvm/src/lib.rs
@@ -19,17 +19,17 @@ mod aptos {
     use sov_modules_macros::ModuleInfo;
     use sov_state::WorkingSet;
 
-    use aptos_types::transaction::{Transaction};
-    use aptos_types::waypoint::{Waypoint};
     use aptos_db::AptosDB;
     use aptos_storage_interface::DbReaderWriter;
+    use aptos_types::transaction::Transaction;
     use aptos_types::validator_signer::ValidatorSigner;
+    use aptos_types::waypoint::Waypoint;
 
+    use aptos_crypto::HashValue;
     use aptos_executor::block_executor::BlockExecutor;
     use aptos_executor::db_bootstrapper::{generate_waypoint, maybe_bootstrap};
     use aptos_executor_types::BlockExecutorTrait;
     use aptos_vm::AptosVM;
-    use aptos_crypto::{HashValue};
     use working_set_aptos_state_view::WorkingSetAptosStateView;
     // use anyhow::{Error};
 
@@ -55,25 +55,24 @@ mod aptos {
         // TODO: this may be redundant with address
         // #[cfg(feature = "aptos-consensus")]
         #[state]
-        pub(crate) validator_signer: sov_state::StateValue<Vec<u8>>, // TODO: fix validator signer incompatability
+        pub(crate) validator_signer: sov_state::StateValue<Vec<u8>>, // TODO: fix validator signer incompatibility
 
         // This is string because we are using transaction.hash: https://github.com/movemntdev/aptos-core/blob/112ad6d8e229a19cfe471153b2fd48f1f22b9684/crates/indexer/src/models/transactions.rs#L31
         // #[cfg(feature = "aptos-consensus")]
         #[state]
-        pub(crate) transactions: sov_state::StateMap<String, Vec<u8>>, // TODO: fix Transaction serialiation incompatability
+        pub(crate) transactions: sov_state::StateMap<String, Vec<u8>>, // TODO: fix Transaction serialiation incompatibility
 
         // #[cfg(feature = "aptos-consensus")]
         #[state]
-        pub(crate) genesis_hash: sov_state::StateValue<Vec<u8>>, // TODO: fix genesis serialiation incompatability
+        pub(crate) genesis_hash: sov_state::StateValue<Vec<u8>>, // TODO: fix genesis serialiation incompatibility
 
         // #[cfg(feature = "aptos-consensus")]
         #[state]
-        pub(crate) waypoint: sov_state::StateValue<String>, // TODO: fix waypoint serialiation incompatability
+        pub(crate) waypoint: sov_state::StateValue<String>, // TODO: fix waypoint serialiation incompatibility
 
         // #[cfg(feature = "aptos-consensus")]
         #[state]
-        pub(crate) known_version : sov_state::StateValue<u64>,
-
+        pub(crate) known_version: sov_state::StateValue<u64>,
     }
 
     impl<C: sov_modules_api::Context> sov_modules_api::Module for AptosVm<C> {
@@ -88,9 +87,7 @@ mod aptos {
             config: &Self::Config,
             working_set: &mut WorkingSet<C::Storage>,
         ) -> Result<(), Error> {
-
             Ok(self.init_module(config, working_set)?)
-
         }
 
         fn call(
@@ -99,133 +96,95 @@ mod aptos {
             context: &Self::Context,
             working_set: &mut WorkingSet<C::Storage>,
         ) -> Result<sov_modules_api::CallResponse, Error> {
-
             Ok(self.execute_call(msg.serialized_txs, context, working_set)?)
-
         }
     }
 
- 
-
     impl<C: sov_modules_api::Context> AptosVm<C> {
-
-        pub (crate) fn get_validator_signer(
+        pub(crate) fn get_validator_signer(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            ValidatorSigner, 
-            Error
-        > {
-            
-            let serialized_validator_signer = self.validator_signer.get(working_set).ok_or(
-                anyhow::Error::msg("Validator signer is not set.")
-            )?;
+        ) -> Result<ValidatorSigner, Error> {
+            let serialized_validator_signer = self
+                .validator_signer
+                .get(working_set)
+                .ok_or(anyhow::Error::msg("Validator signer is not set."))?;
 
             // TODO: seems redundant, but error types are different
-            Ok(serde_json::from_slice::<ValidatorSigner>(&serialized_validator_signer).expect(
-                "Failed to deserialize validator signer"
-            ))
-
+            Ok(
+                serde_json::from_slice::<ValidatorSigner>(&serialized_validator_signer)
+                    .expect("Failed to deserialize validator signer"),
+            )
         }
 
-        pub (crate) fn get_genesis_hash(
+        pub(crate) fn get_genesis_hash(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            HashValue, 
-            Error
-        > {
-            
-            let serialized_genesis_hash = self.genesis_hash.get(working_set).ok_or(
-                anyhow::Error::msg("Serialized genesis hash is not set.")
-            )?;
+        ) -> Result<HashValue, Error> {
+            let serialized_genesis_hash = self
+                .genesis_hash
+                .get(working_set)
+                .ok_or(anyhow::Error::msg("Serialized genesis hash is not set."))?;
 
             // TODO: seems redundant, but error types are different
-            Ok(HashValue::from_slice(serialized_genesis_hash).expect(
-                "Failed to deserialize genesis hash"
-            ))
-
+            Ok(HashValue::from_slice(serialized_genesis_hash)
+                .expect("Failed to deserialize genesis hash"))
         }
 
-        pub (crate) fn get_waypoint(
+        pub(crate) fn get_waypoint(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            Waypoint, 
-            Error
-        > {
-            
-            let serialized_waypoint = self.waypoint.get(working_set).ok_or(
-                anyhow::Error::msg("Serialized waypoint hash is not set.")
-            )?;
+        ) -> Result<Waypoint, Error> {
+            let serialized_waypoint = self
+                .waypoint
+                .get(working_set)
+                .ok_or(anyhow::Error::msg("Serialized waypoint hash is not set."))?;
 
             println!("serialized_waypoint: {:?}", serialized_waypoint);
 
             // TODO: seems redundant, but error types are different
-            Ok(Waypoint::from_str(serialized_waypoint.as_str()).expect(
-                "Failed to deserialize waypoint"
-            ))
-
+            Ok(Waypoint::from_str(serialized_waypoint.as_str())
+                .expect("Failed to deserialize waypoint"))
         }
 
-        pub (crate) fn get_known_version(
+        pub(crate) fn get_known_version(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            u64, 
-            Error
-        > {
-            
-            let known_version = self.known_version.get(working_set).ok_or(
-                anyhow::Error::msg("Serialized waypoint hash is not set.")
-            )?;
+        ) -> Result<u64, Error> {
+            let known_version = self
+                .known_version
+                .get(working_set)
+                .ok_or(anyhow::Error::msg("Serialized waypoint hash is not set."))?;
 
             Ok(known_version)
-
         }
 
         pub(crate) fn get_db(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            DbReaderWriter, 
-            Error
-        > {
+        ) -> Result<DbReaderWriter, Error> {
+            let path = self
+                .db_path
+                .get(working_set)
+                .ok_or(anyhow::Error::msg("Database path is not set."))?;
 
-            let path = self.db_path.get(working_set).ok_or(
-                anyhow::Error::msg("Database path is not set.")
-            )?;
- 
             Ok(DbReaderWriter::new(AptosDB::new_for_sov(path.as_str())))
-
         }
 
         pub(crate) fn get_executor(
             &self,
             working_set: &mut WorkingSet<C::Storage>,
-        ) -> Result<
-            BlockExecutor<AptosVM>, 
-            Error
-        > {
-
+        ) -> Result<BlockExecutor<AptosVM>, Error> {
             let db = self.get_db(working_set)?;
             Ok(BlockExecutor::new(db.clone()))
-
         }
 
         pub(crate) fn get_aptos_vm(
             &self,
-            working_set: &mut WorkingSet<C::Storage>
-        ) -> Result<
-            AptosVM, 
-            Error
-        > {
-
+            working_set: &mut WorkingSet<C::Storage>,
+        ) -> Result<AptosVM, Error> {
             AptosVM::new();
             unimplemented!("Get aptos vm is not implemented!")
-
         }
-
     }
-
 }
--- rollups/sovereign-labs/movement-sovereign-labs-rollup/util/working-set-change-set-publisher/src/lib.rs
+++ rollups/sovereign-labs/movement-sovereign-labs-rollup/util/working-set-change-set-publisher/src/lib.rs
@@ -1,74 +1,80 @@
+use aptos_types::access_path::AccessPath;
 use aptos_types::account_config::resources;
 use ethers_core::k256::sha2::digest::typenum::Mod;
-use move_core_types::{effects::{ChangeSet, Op}, language_storage::ModuleId};
-use sov_state::{WorkingSet, StateMap};
+use move_core_types::{
+    effects::{ChangeSet, Op},
+    language_storage::ModuleId,
+};
 use sov_movevm_types::identifiers::AccessPathWrapper;
+use sov_state::{StateMap, WorkingSet};
 use std::cell::RefCell;
-use aptos_types::access_path::AccessPath;
 
-/// This is an implmenetantion of remote cache which will be back of TransactionDataCache
+/// This is an implementation of remote cache which will be back of TransactionDataCache
 /// review? I used RefCell but not sure if it can be secure.
 pub struct ChangeSetPublisher<'a, C: sov_modules_api::Context> {
-  pub remote_cache: sov_state::StateMap<AccessPathWrapper, Vec<u8>>,
-  pub working_set: RefCell<&'a mut WorkingSet<C::Storage>>,
+    pub remote_cache: sov_state::StateMap<AccessPathWrapper, Vec<u8>>,
+    pub working_set: RefCell<&'a mut WorkingSet<C::Storage>>,
 }
 
 impl<'a, C: sov_modules_api::Context> ChangeSetPublisher<'a, C> {
-  pub fn new(
-      remote_cache: StateMap<AccessPathWrapper, Vec<u8>>,
-      working_set: &'a mut WorkingSet<C::Storage>,
-  ) -> Self {
-      Self {
-          remote_cache,
-          working_set: RefCell::new(working_set),
-      }
-  }
-
-  pub fn publish(&self, change_set: ChangeSet) -> Result<(), anyhow::Error> {
-
-    let mut working_set = self.working_set.borrow_mut();
-    for (account_address, account_changes) in change_set.accounts().iter() {
-
-      let (modules, resources) = account_changes.to_owned().into_inner(); // owning better than cloning
-
-      // modules
-      for (path, op) in modules.iter() {
-        let ap = AccessPath::code_access_path(ModuleId::new(
-          account_address.clone(),
-          path.clone()
-        ));
-        match op {
-          Op::New(data) => {
-            self.remote_cache.set(&AccessPathWrapper::new(ap), data, &mut working_set);
-          },
-          Op::Modify(data) => {
-            self.remote_cache.set(&AccessPathWrapper::new(ap), data, &mut working_set);
-          },
-          Op::Delete => {
-            self.remote_cache.remove(&AccessPathWrapper::new(ap),  &mut working_set);
-          },
+    pub fn new(
+        remote_cache: StateMap<AccessPathWrapper, Vec<u8>>,
+        working_set: &'a mut WorkingSet<C::Storage>,
+    ) -> Self {
+        Self {
+            remote_cache,
+            working_set: RefCell::new(working_set),
         }
-      }
-
-      // resources
-      for (path, op) in resources.iter() {
-        let ap = AccessPath::resource_access_path(*account_address, path.clone().to_owned())?; // struct tag is actually somwhat large so avoiding clone may be useful
-        match op {
-          Op::New(data) => {
-            self.remote_cache.set(&AccessPathWrapper::new(ap), data, &mut working_set);
-          },
-          Op::Modify(data) => {
-            self.remote_cache.set(&AccessPathWrapper::new(ap), data, &mut working_set);
-          },
-          Op::Delete => {
-            self.remote_cache.remove(&AccessPathWrapper::new(ap),  &mut working_set);
-          },
-        }
-      }
-
     }
 
-    Ok(())
+    pub fn publish(&self, change_set: ChangeSet) -> Result<(), anyhow::Error> {
+        let mut working_set = self.working_set.borrow_mut();
+        for (account_address, account_changes) in change_set.accounts().iter() {
+            let (modules, resources) = account_changes.to_owned().into_inner(); // owning better than cloning
 
-  }
+            // modules
+            for (path, op) in modules.iter() {
+                let ap = AccessPath::code_access_path(ModuleId::new(
+                    account_address.clone(),
+                    path.clone(),
+                ));
+                match op {
+                    Op::New(data) => {
+                        self.remote_cache
+                            .set(&AccessPathWrapper::new(ap), data, &mut working_set);
+                    }
+                    Op::Modify(data) => {
+                        self.remote_cache
+                            .set(&AccessPathWrapper::new(ap), data, &mut working_set);
+                    }
+                    Op::Delete => {
+                        self.remote_cache
+                            .remove(&AccessPathWrapper::new(ap), &mut working_set);
+                    }
+                }
+            }
+
+            // resources
+            for (path, op) in resources.iter() {
+                let ap =
+                    AccessPath::resource_access_path(*account_address, path.clone().to_owned())?; // struct tag is actually somewhat large so avoiding clone may be useful
+                match op {
+                    Op::New(data) => {
+                        self.remote_cache
+                            .set(&AccessPathWrapper::new(ap), data, &mut working_set);
+                    }
+                    Op::Modify(data) => {
+                        self.remote_cache
+                            .set(&AccessPathWrapper::new(ap), data, &mut working_set);
+                    }
+                    Op::Delete => {
+                        self.remote_cache
+                            .remove(&AccessPathWrapper::new(ap), &mut working_set);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
